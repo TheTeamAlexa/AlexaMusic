309335cf80a10b3820964e62dcbef8f0085dfc72
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2022-08-18 18:29:47 +0500

    Alexa Music V2

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
+# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/TheTeamAlexa >.
 # A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
 
 # Kanged By © @Dr_Asad_Ali
