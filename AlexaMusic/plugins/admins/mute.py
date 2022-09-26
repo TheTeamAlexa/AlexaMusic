7abf610161f45a4852e5e15f2f48af4b48cd21e5
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2022-09-27 03:11:57 +0500

    Alexa Music

--- AlexaMusic/plugins/admins/mute.py
+++ AlexaMusic/plugins/admins/mute.py
@@ -31,7 +31,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
         return await message.reply_text(_["general_2"])
     if await is_muted(chat_id):
-        return await message.reply_text(_["admin_5"])
+        return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
     await Alexa.mute_stream(chat_id)
-    await message.reply_text(_["admin_6"].format(message.from_user.mention))
+    await message.reply_text(_["admin_6"].format(message.from_user.mention), disable_web_page_preview=True)
