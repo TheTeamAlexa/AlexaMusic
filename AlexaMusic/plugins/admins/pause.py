cd7e9e17ebe45afdc2bd577620886cf9547a831a
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2022-09-27 03:17:28 +0500

    Alexa Music V2

--- AlexaMusic/plugins/admins/pause.py
+++ AlexaMusic/plugins/admins/pause.py
@@ -31,7 +31,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     if not await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_1"])
+        return await message.reply_text(_["admin_1"], disable_web_page_preview=True)
     await music_off(chat_id)
     await Alexa.pause_stream(chat_id)
-    await message.reply_text(_["admin_2"].format(message.from_user.mention))
+    await message.reply_text(_["admin_2"].format(message.from_user.mention), disable_web_page_preview=True)
