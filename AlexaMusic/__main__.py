f6ec59611abef0f186d57353256b5f5e68b75bb3
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2022-12-01 04:38:12 +0500

    Update __main__.py

--- AlexaMusic/__main__.py
+++ AlexaMusic/__main__.py
@@ -35,7 +35,7 @@ async def init():
         and not config.STRING5
     ):
         LOGGER("AlexaMusic").error(
-            "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
+            "Add Pyrogram string session and then try..."
         )
     try:
         users = await get_gbanned()
@@ -56,16 +56,16 @@ async def init():
         await Alexa.stream_call("https://telegra.ph/file/b60b80ccb06f7a48f68b5.mp4")
     except NoActiveGroupCall:
         LOGGER("AlexaMusic").error(
-            "[ERROR] - \n\nHey Baby, firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
+            "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
         )
         sys.exit()
     except:
         pass
     await Alexa.decorators()
-    LOGGER("AlexaMusic").info("Music Bot Started Successfully ❣️")
+    LOGGER("AlexaMusic").info("Music Bot Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("AlexaMusic").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
+    LOGGER("AlexaMusic").info("Stopping Music Bot")
