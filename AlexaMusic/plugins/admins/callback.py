ebd32ff6e9316efcb818f834d5b699d09a0c5aab
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-03-07 12:11:56 +0500

    Update @TheTeamAlexa

--- AlexaMusic/plugins/admins/callback.py
+++ AlexaMusic/plugins/admins/callback.py
@@ -194,7 +194,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         user = check[0]["by"]
         streamtype = check[0]["streamtype"]
         videoid = check[0]["vidid"]
-        user_id = check[0]["user_id"]
+        user_id = CallbackQuery.message.from_user.id
         theme = await check_theme(chat_id)
         status = True if str(streamtype) == "video" else None
         db[chat_id][0]["played"] = 0
@@ -208,8 +208,8 @@ async def del_back_playlist(client, CallbackQuery, _):
                 await Alexa.skip_stream(chat_id, link, video=status)
             except Exception:
                 return await CallbackQuery.message.reply_text(_["call_9"])
+            theme = await check_theme(chat_id)    
             button = telegram_markup(_, chat_id)
-            theme = await check_theme(chat_id)
             img = await gen_thumb(videoid, user_id, theme)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
@@ -239,8 +239,8 @@ async def del_back_playlist(client, CallbackQuery, _):
                 await Alexa.skip_stream(chat_id, file_path, video=status)
             except Exception:
                 return await mystic.edit_text(_["call_9"])
+            theme = await check_theme(chat_id)    
             button = stream_markup(_, videoid, chat_id)
-            theme = await check_theme(chat_id)
             img = await gen_thumb(videoid, user_id, theme)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
@@ -296,8 +296,8 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "tg"
             else:
-                button = stream_markup(_, videoid, chat_id)
                 theme = await check_theme(chat_id)
+                button = stream_markup(_, videoid, chat_id)
                 img = await gen_thumb(videoid, user_id, theme)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
