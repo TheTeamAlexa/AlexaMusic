cab0d5b98c514100931cdc0c67f6b0d71b2f37e7
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-04-26 15:05:17 +0500

    Broadcast Subscription

--- AlexaMusic/plugins/bot/help.py
+++ AlexaMusic/plugins/bot/help.py
@@ -88,3 +88,5 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
     elif cb == "hb7":
         await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
+    elif cb == "hb8":
+        await CallbackQuery.edit_message_text(helpers.HELP_8, reply_markup=keyboard)
