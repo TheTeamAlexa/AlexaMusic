e4855cb148c5b6361c8a93a834ea0631bc4f2085
kenta9900 <142348839+kenta9900@users.noreply.github.com>
2023-08-21 02:59:38 +0700

    Update active.py

--- AlexaMusic/plugins/tools/active.py
+++ AlexaMusic/plugins/tools/active.py
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Copyright (C) 2021-2022 by NIRVANA
+# A Powerful Music Bot Property Of Rocks NIRVANA
 
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alisha © Alexa © Yukki
+# Kanged By © @exsaezz
+# Rocks © NIRVANA
+# Owner alexsacei
+# 
+# All rights reserved. © alexsacei © NIRVANA
 
 
 from pyrogram import filters
