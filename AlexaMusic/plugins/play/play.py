a9357aac3b4d3289ad2a3d4a5c48726b943718f1
kenta9900 <142348839+kenta9900@users.noreply.github.com>
2023-08-21 03:45:17 +0700

    Update play.py

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -60,7 +60,7 @@ async def play_commnd(
 ):
     if not await is_served_user(message.from_user.id):
         await message.reply_text(
-            text="😢 ᴅᴇᴀʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀ ᴠᴇʀɪғɪᴇᴅ ᴀᴛ ᴀʟᴇxᴀ ᴅᴀᴛᴀʙᴀsᴇ.\n☔ ᴘʟᴇᴀsᴇ ᴜsᴇ /verify ᴛᴏ ᴠᴇʀɪғʏ ʏᴏᴜʀsᴇʟғ ᴀᴛ ᴀʟᴇxᴀ ᴅᴀᴛᴀʙᴀsᴇ.",
+            text="😢 ᴅᴇᴀʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀ ᴠᴇʀɪғɪᴇᴅ ᴀᴛ NIRVANA ᴅᴀᴛᴀʙᴀsᴇ.\n☔
         )
         return
     mystic = await message.reply_text(
