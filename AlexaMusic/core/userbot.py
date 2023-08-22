e3fc3b2d416cc322d5a133201399adb9a43a8835
kenta9900 <142348839+kenta9900@users.noreply.github.com>
2023-08-22 07:19:16 +0700

    Update userbot.py

--- AlexaMusic/core/userbot.py
+++ AlexaMusic/core/userbot.py
@@ -59,7 +59,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Alexa_Help")
+                await self.one.join_chat("Alexsacei_Help")
             except:
                 pass
             assistants.append(1)
@@ -85,7 +85,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Alexa_Help")
+                await self.two.join_chat("alexsacei_help")
             except:
                 pass
             assistants.append(2)
@@ -111,7 +111,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Alexa_Help")
+                await self.three.join_chat("Alexsacei_Help")
             except:
                 pass
             assistants.append(3)
@@ -163,7 +163,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Alexa_Help")
+                await self.five.join_chat("Alexsacei_Help")
             except:
                 pass
             assistants.append(5)
