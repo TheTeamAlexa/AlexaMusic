5c69bc9afa8eabec032e45a887887f56cfd9ebc8
kenta9900 <142348839+kenta9900@users.noreply.github.com>
2023-08-22 14:07:46 +0700

    Update onoff.py

--- AlexaMusic/utils/database/onoff.py
+++ AlexaMusic/utils/database/onoff.py
@@ -1,13 +1,13 @@
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# Without Credit (Mother Fucker)
-# Rocks © @Dr_Asad_Ali © Rocks
-# Owner Asad Ali
-# Harshit Sharma
+# A Powerful Music Bot Property Of Rocks NIRVANA
+# Without Credit (Mother Fucker🖕🏻)
+# Rocks © @exsaezz © Rocks
+# Owner Alexsacei
+# Alexsacei
 
 
 from typing import Dict, List, Union
 
-from AlexaMusic.core.mongo import mongodb
+from AlexsaceiMusic.core.mongo import mongodb
 
 
 onoffdb = mongodb.onoffper
