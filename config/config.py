4aa0b145df8eccd9f127f3dab5d9bd4244d8f684
kenta9900 <142348839+kenta9900@users.noreply.github.com>
2023-08-22 07:23:20 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,16 +36,16 @@ BOT_ID = getenv("BOT_ID")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/TheTeamAlexa/AlexaMusic",
+
+
+
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alexa_BotUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Alexa_Help")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/justenoughtz")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/groupjawanusantara")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
