cdae768229c58db28ee15e683d7017f3ae5531b0
kenta9900 <142348839+kenta9900@users.noreply.github.com>
2023-08-22 13:13:33 +0700

    Update misc.py

--- AlexaMusic/misc.py
+++ AlexaMusic/misc.py
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2021-2022 by Alexsacei@Github, < https://github.com/kenta9900 >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# A Powerful Music Bot Property Of Rocks NIRVANA
 
 # Kanged By © @exsaezz
 # Rocks © @groupjawanusantara
