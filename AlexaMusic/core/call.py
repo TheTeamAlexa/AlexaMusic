94080b6c045c081e2bde03cef2600bc3d6fb6479
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-11-27 19:47:19 +0500

    Alexa Music

--- AlexaMusic/core/call.py
+++ AlexaMusic/core/call.py
@@ -1,7 +1,13 @@
-#
-# Copyright (C) 2021-2022 by Alexa_Help Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# All rights reserved. © Alisha © Alexa © Yukki
+# Copyright (C) 2023 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2023 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
 
 
 import asyncio
