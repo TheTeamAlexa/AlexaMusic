1b1bbbe1da18be8b6c832928ad4087ec818db70d
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-11-27 20:21:31 +0500

    Pain Subscription Added

--- AlexaMusic/plugins/modules/paidsub.py
+++ AlexaMusic/plugins/modules/paidsub.py
@@ -1,1 +1,24 @@
+# Copyright (C) 2023 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2023 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+import asyncio
+from AlexaMusic import app
+from pyrogram import Client, filters
+from datetime import datetime, timedelta
+from pyrogram.errors import FloodWait
+from AlexaMusic.core.mongo import db as alexa
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from AlexaMusic.utils.database import get_served_users, get_served_chats
+
+
+OWNER_ID = 6174058850
 
