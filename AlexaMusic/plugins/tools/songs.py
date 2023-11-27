12cf30ab71811b58f0cb6c0e1a8273bf45549957
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-11-27 20:16:42 +0500

    Alexa Music

--- AlexaMusic/plugins/tools/songs.py
+++ AlexaMusic/plugins/tools/songs.py
@@ -1,9 +1,12 @@
+# Copyright (C) 2023 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
 """"
-Alexa is a Telegram Audio and video streaming bot 
-Copyright (c) 2022 -present Team=Alexa <https://github.com/TheTeamAlexa>
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2023 -present Team=Alexa <https://github.com/TheTeamAlexa>
 
 This program is free software: you can redistribute it and can modify
-as you want.
+as you want or you can collabe if you have new ideas.
 """
 
 import os
