7431dc6dc6cc18b04b9b6ad81fb1d61b77a5c3bc
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-11-27 19:57:56 +0500

    Alexa Music

--- AlexaMusic/plugins/bot/help.py
+++ AlexaMusic/plugins/bot/help.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2023 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2023 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
 from typing import Union
 
 from pyrogram import filters, types
