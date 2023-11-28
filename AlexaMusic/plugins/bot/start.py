4789fc1c0c98da94e1a4b687aeb9f3cd4fc7d936
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-11-28 20:37:51 +0500

    Updated To Pyro 2

--- AlexaMusic/plugins/bot/start.py
+++ AlexaMusic/plugins/bot/start.py
@@ -13,7 +13,7 @@ as you want or you can collabe if you have new ideas.
 import asyncio
 
 from pyrogram import filters
-from pyrogram import Client, filters
+from pyrogram import enums, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
@@ -250,7 +250,7 @@ async def welcome(client, message: Message):
             _ = get_string(language)
             if member.id == app.id:
                 chat_type = message.chat.type
-                if chat_type != "supergroup":
+                if chat_type != enums.ChatType.SUPERGROUP:
                     await message.reply_text(_["start_6"])
                     return await app.leave_chat(message.chat.id)
                 if chat_id in await blacklisted_chats():
