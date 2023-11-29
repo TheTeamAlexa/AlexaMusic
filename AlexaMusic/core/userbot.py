99f7d712eba2cba9c4db524247ba46c59358863d
˹ᴋʀɪsʜɴᴀ ˼ <officialkrishna7618@gmail.com>
2023-11-29 11:14:51 +0530

    Update userbot.py

--- AlexaMusic/core/userbot.py
+++ AlexaMusic/core/userbot.py
@@ -25,30 +25,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
+            name="AlexaString1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
+            name="AlexaString2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
+            name="AlexaString3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
+            name="AlexaString4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
+            name="AlexaString5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
