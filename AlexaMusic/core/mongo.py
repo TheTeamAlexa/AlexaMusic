43413f4be3cced2937ce96ff1c44de1ec1f73691
˹ᴋʀɪsʜɴᴀ ˼ <officialkrishna7618@gmail.com>
2024-01-10 21:19:44 +0530

    remove useless things

--- AlexaMusic/core/mongo.py
+++ AlexaMusic/core/mongo.py
@@ -11,38 +11,10 @@ as you want or you can collabe if you have new ideas.
 
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
-from pyrogram import Client
 
 import config
 
-from ..logging import LOGGER
-
-TEMP_MONGODB = ""
-
-
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning("No MONGO DB URL found.")
-    temp_client = Client(
-        "Alexa",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Alexa
-    pymongodb = _mongo_sync_.Alexa
-
-## Database For Broadcast Subscription By Team Alexa
-
-MONGODB_CLI = MongoClient(config.MONGO_DB_URI)
-db = MONGODB_CLI["subscriptions"]
+_mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+_mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+mongodb = _mongo_async_.Alexa
+pymongodb = _mongo_sync_.Alexa
