68005103723e0fbbfc937fe7fec66c7778e624e9
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2024-01-13 01:04:14 +0500

    Logging to NTgCalls

--- AlexaMusic/logging.py
+++ AlexaMusic/logging.py
@@ -25,7 +25,8 @@ logging.basicConfig(
 )
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("ntgcalls").setLevel(logging.ERROR)
+
 
 
 def LOGGER(name: str) -> logging.Logger:
