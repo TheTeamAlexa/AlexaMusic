8f12899cfaf2bf6bad7215f17ed5c6840ea6be26
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2024-01-13 01:46:24 +0500

    Changes Revert Back

--- AlexaMusic/core/call.py
+++ AlexaMusic/core/call.py
@@ -72,7 +72,7 @@ class Call(PyTgCalls):
         self.userbot1 = Client(
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            session_name=str(config.STRING1),
+            session_string=str(config.STRING1),
         )
         self.one = PyTgCalls(
             self.userbot1,
