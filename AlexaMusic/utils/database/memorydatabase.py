80156d9becd795911eda52898a38c19b1dac882d
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-01-12 22:21:33 +0530

    No Sound Fixed

--- AlexaMusic/utils/database/memorydatabase.py
+++ AlexaMusic/utils/database/memorydatabase.py
@@ -459,13 +459,11 @@ async def maintenance_on():
 
 # Audio Video Limit
 
-from pytgcalls.types.input_stream.quality import (
-    HighQualityAudio,
-    HighQualityVideo,
-    LowQualityAudio,
-    LowQualityVideo,
-    MediumQualityAudio,
-    MediumQualityVideo,
+from pytgcalls.types import (
+    AudioParameters, 
+    AudioQuality, 
+    VideoParameters, 
+    VideoQuality
 )
 
 
@@ -479,43 +477,42 @@ async def save_video_bitrate(chat_id: int, bitrate: str):
 
 async def get_aud_bit_name(chat_id: int) -> str:
     mode = audio.get(chat_id)
-    if not mode:
-        return "High"
-    return mode
+    return "HIGH" if not mode else mode
 
 
 async def get_vid_bit_name(chat_id: int) -> str:
     mode = video.get(chat_id)
-    if not mode:
-        if PRIVATE_BOT_MODE == str(True):
-            return "High"
-        else:
-            return "Medium"
-    return mode
+    return "FHD_1080p" if not mode else mode
 
 
 async def get_audio_bitrate(chat_id: int) -> str:
     mode = audio.get(chat_id)
     if not mode:
-        return MediumQualityAudio()
-    if str(mode) == "High":
-        return HighQualityAudio()
-    elif str(mode) == "Medium":
-        return MediumQualityAudio()
-    elif str(mode) == "Low":
-        return LowQualityAudio()
+        return AudioParameters.from_quality(AudioQuality.STUDIO)
+    if str(mode) == "STUDIO":
+        return AudioParameters.from_quality(AudioQuality.STUDIO)
+    elif str(mode) == "HIGH":
+        return AudioParameters.from_quality(AudioQuality.HIGH)
+    elif str(mode) == "MEDIUM":
+        return AudioParameters.from_quality(AudioQuality.MEDIUM)
+    elif str(mode) == "LOW":
+        return AudioParameters.from_quality(AudioQuality.LOW)
 
 
 async def get_video_bitrate(chat_id: int) -> str:
     mode = video.get(chat_id)
     if not mode:
         if PRIVATE_BOT_MODE == str(True):
-            return HighQualityVideo()
+            return VideoParameters.from_quality(VideoQuality.FHD_1080p)
         else:
-            return MediumQualityVideo()
-    if str(mode) == "High":
-        return HighQualityVideo()
-    elif str(mode) == "Medium":
-        return MediumQualityVideo()
-    elif str(mode) == "Low":
-        return LowQualityVideo()
+            return VideoParameters.from_quality(VideoQuality.HD_720p)
+    if str(mode) == "QHD_2K":
+        return VideoParameters.from_quality(VideoQuality.QHD_2K)
+    elif str(mode) == "FHD_1080p":
+        return VideoParameters.from_quality(VideoQuality.FHD_1080p)
+    elif str(mode) == "HD_720p":
+        return VideoParameters.from_quality(VideoQuality.HD_720p)
+    elif str(mode) == "SD_480p":
+        return VideoParameters.from_quality(VideoQuality.SD_480p)
+    elif str(mode) == "SD_360p":
+        return VideoParameters.from_quality(VideoQuality.SD_360p)
