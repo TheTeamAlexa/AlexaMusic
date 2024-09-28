113f4fcf958614ad154a14303852455b033bfb27
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-09-28 19:13:16 +0530

    Quality

--- AlexaMusic/utils/database/memorydatabase.py
+++ AlexaMusic/utils/database/memorydatabase.py
@@ -459,14 +459,7 @@ async def maintenance_on():
 
 # Audio Video Limit
 
-from pytgcalls.types.input_stream.quality import (
-    HighQualityAudio,
-    HighQualityVideo,
-    LowQualityAudio,
-    LowQualityVideo,
-    MediumQualityAudio,
-    MediumQualityVideo,
-)
+from pytgcalls.types import AudioQuality, VideoQuality
 
 
 async def save_audio_bitrate(chat_id: int, bitrate: str):
@@ -480,42 +473,37 @@ async def save_video_bitrate(chat_id: int, bitrate: str):
 async def get_aud_bit_name(chat_id: int) -> str:
     mode = audio.get(chat_id)
     if not mode:
-        return "High"
+        return "HIGH"
     return mode
 
 
 async def get_vid_bit_name(chat_id: int) -> str:
     mode = video.get(chat_id)
-    if not mode:
-        if PRIVATE_BOT_MODE == str(True):
-            return "High"
-        else:
-            return "Medium"
-    return mode
+    return "LOW" if not mode else mode
 
 
 async def get_audio_bitrate(chat_id: int) -> str:
     mode = audio.get(chat_id)
     if not mode:
-        return MediumQualityAudio()
-    if str(mode) == "High":
-        return HighQualityAudio()
-    elif str(mode) == "Medium":
-        return MediumQualityAudio()
-    elif str(mode) == "Low":
-        return LowQualityAudio()
+        return AudioQuality.HIGH
+    if str(mode) == "HIGH":
+        return AudioQuality.STUDIO
+    elif str(mode) == "MEDIUM":
+        return AudioQuality.HIGH
+    elif str(mode) == "LOW":
+        return AudioQuality.LOW()
 
 
 async def get_video_bitrate(chat_id: int) -> str:
     mode = video.get(chat_id)
     if not mode:
         if PRIVATE_BOT_MODE == str(True):
-            return HighQualityVideo()
+            return VideoQuality.FHD_1080p
         else:
-            return MediumQualityVideo()
-    if str(mode) == "High":
-        return HighQualityVideo()
-    elif str(mode) == "Medium":
-        return MediumQualityVideo()
-    elif str(mode) == "Low":
-        return LowQualityVideo()
+            return VideoQuality.SD_480p
+    if str(mode) == "HIGH":
+        return VideoQuality.QHD_2K
+    elif str(mode) == "MEDIUM":
+        return VideoQuality.FHD_1080p
+    elif str(mode) == "LOW":
+        return VideoQuality.SD_480p
