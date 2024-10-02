42838165939294a1d600e3056cb85a1e2f14f5e4
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-10-02 21:37:27 +0530

    Revert ""ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs" Fix"
    This reverts commit c98bd9eb3a629b195e5e14df5aee2285f2889a85.

--- AlexaMusic/plugins/bot/help.py
+++ AlexaMusic/plugins/bot/help.py
@@ -18,6 +18,7 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
 from AlexaMusic import app
+from AlexaMusic.misc import SUDOERS
 from AlexaMusic.utils import help_pannel
 from AlexaMusic.utils.database import get_lang, is_commanddelete_on
 from AlexaMusic.utils.decorators.language import LanguageStart, languageCB
@@ -73,6 +74,18 @@ async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = help_back_markup(_)
+    if cb == "hb5":
+        if CallbackQuery.from_user.id not in SUDOERS:
+            return await CallbackQuery.answer(
+                "ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs", show_alert=True
+            )
+        else:
+            await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
+            return await CallbackQuery.answer()
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
     if cb == "hb1":
         await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
     elif cb == "hb2":
@@ -81,8 +94,6 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_3, reply_markup=keyboard)
     elif cb == "hb4":
         await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
-    elif cb == "hb5":
-        await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
     elif cb == "hb7":
         await CallbackQuery.edit_message_text(helpers.HELP_7, reply_markup=keyboard)
     elif cb == "hb8":
