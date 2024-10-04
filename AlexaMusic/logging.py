413e1265371c936c3286821cf76affba4525c5e2
KSK OP <123794242+KSKOP69@users.noreply.github.com>
2024-10-04 14:15:41 +0530

    pymongo

--- AlexaMusic/logging.py
+++ AlexaMusic/logging.py
@@ -26,7 +26,7 @@ logging.basicConfig(
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-logging.getLogger("ntgcalls").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("httpx").setLevel(logging.ERROR)
 
 
