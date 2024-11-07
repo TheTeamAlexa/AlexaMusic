95ae53f89b152aee7ff1b0b5dbdbe79025681603
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-11-08 00:33:35 +0530

    Theme 😭

--- AlexaMusic/utils/database/theme.py
+++ AlexaMusic/utils/database/theme.py
@@ -10,32 +10,32 @@ as you want or you can collabe if you have new ideas.
 """
 
 
-from typing import Dict, List, Union
-from AlexaMusic.core.mongo import mongodb
+# from typing import Dict, List, Union
+# from AlexaMusic.core.mongo import mongodb
 
-themedb = mongodb.notes
-
-
-async def _get_theme(chat_id: int) -> Dict[str, int]:
-    _notes = await themedb.find_one({"chat_id": chat_id})
-    if not _notes:
-        return {}
-    return _notes["notes"]
+# themedb = mongodb.notes
 
 
-async def get_theme(chat_id: int, name: str) -> Union[bool, dict]:
-    name = name.lower().strip()
-    _notes = await _get_theme(chat_id)
-    if name in _notes:
-        return _notes[name]
-    else:
-        return False
+# async def _get_theme(chat_id: int) -> Dict[str, int]:
+#     _notes = await themedb.find_one({"chat_id": chat_id})
+#     if not _notes:
+#         return {}
+#     return _notes["notes"]
 
 
-async def save_theme(chat_id: int, name: str, note: dict):
-    name = name.lower().strip()
-    _notes = await _get_theme(chat_id)
-    _notes[name] = note
-    await themedb.update_one(
-        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
-    )
+# async def get_theme(chat_id: int, name: str) -> Union[bool, dict]:
+#     name = name.lower().strip()
+#     _notes = await _get_theme(chat_id)
+#     if name in _notes:
+#         return _notes[name]
+#     else:
+#         return False
+
+
+# async def save_theme(chat_id: int, name: str, note: dict):
+#     name = name.lower().strip()
+#     _notes = await _get_theme(chat_id)
+#     _notes[name] = note
+#     await themedb.update_one(
+#         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
+#     )
--- AlexaMusic/utils/theme.py
+++ AlexaMusic/utils/theme.py
@@ -10,27 +10,27 @@ as you want or you can collabe if you have new ideas.
 """
 
 
-import random
-from AlexaMusic.utils.database import get_theme
+# import random
+# from AlexaMusic.utils.database import get_theme
 
-themes = [
-    "alexa1",
-    "alexa2",
-    "alexa3",
-    "alexa4",
-    "alexa5",
-    "alexa6",
-    "alexa7",
-    "alexa8",
-]
+# themes = [
+#     "alexa1",
+#     "alexa2",
+#     "alexa3",
+#     "alexa4",
+#     "alexa5",
+#     "alexa6",
+#     "alexa7",
+#     "alexa8",
+# ]
 
 
-async def check_theme(chat_id: int):
-    _theme = await get_theme(chat_id, "theme")
-    if not _theme:
-        theme = random.choice(themes)
-    else:
-        theme = _theme["theme"]
-        if theme == "Random":
-            theme = random.choice(themes)
-    return theme
+# async def check_theme(chat_id: int):
+#     _theme = await get_theme(chat_id, "theme")
+#     if not _theme:
+#         theme = random.choice(themes)
+#     else:
+#         theme = _theme["theme"]
+#         if theme == "Random":
+#             theme = random.choice(themes)
+#     return theme
