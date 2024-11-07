f3b410d98a70f8c4c6063936b76b5897f322b869
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-11-08 00:42:18 +0530

    OK Good Now !

--- AlexaMusic/platforms/Telegram.py
+++ AlexaMusic/platforms/Telegram.py
@@ -12,24 +12,19 @@ as you want or you can collabe if you have new ideas.
 import asyncio
 import os
 import time
-from datetime import datetime, timedelta
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from config import MUSIC_BOT_NAME, lyrical
 from AlexaMusic import app
-
-from ..utils.formatters import convert_bytes, get_readable_time, seconds_to_min
-
-downloader = {}
+from AlexaMusic.utils.formatters import check_duration, convert_bytes, get_readable_time, seconds_to_min
 
 
 class TeleAPI:
     def __init__(self):
         self.chars_limit = 4096
-        self.sleep = config.TELEGRAM_DOWNLOAD_EDIT_SLEEP
+        self.sleep = 5
 
     async def send_split_text(self, message, string):
         n = self.chars_limit
@@ -38,25 +33,19 @@ class TeleAPI:
         for x in out:
             if j <= 2:
                 j += 1
-                await message.reply_text(x)
+                await message.reply_text(x, disable_web_page_preview=True)
         return True
 
     async def get_link(self, message):
-        if message.chat.username:
-            link = f"https://t.me/{message.chat.username}/{message.reply_to_message.id}"
-        else:
-            xf = str((message.chat.id))[4:]
-            link = f"https://t.me/c/{xf}/{message.reply_to_message.id}"
-        return link
+        return message.link
 
     async def get_filename(self, file, audio: Union[bool, str] = None):
         try:
             file_name = file.file_name
             if file_name is None:
-                file_name = "Telegram Audio File" if audio else "Telegram Video File"
-
+                file_name = "ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴅɪᴏ" if audio else "ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ"
         except:
-            file_name = "Telegram Audio File" if audio else "Telegram Video File"
+            file_name = "ᴛᴇʟᴇɢʀᴀᴍ ᴀᴜᴅɪᴏ" if audio else "ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ"
         return file_name
 
     async def get_duration(self, file):
@@ -64,6 +53,19 @@ class TeleAPI:
             dur = seconds_to_min(file.duration)
         except:
             dur = "Unknown"
+        return dur
+
+    async def get_duration(self, filex, file_path):
+        try:
+            dur = seconds_to_min(filex.duration)
+        except:
+            try:
+                dur = await asyncio.get_event_loop().run_in_executor(
+                    None, check_duration, file_path
+                )
+                dur = seconds_to_min(dur)
+            except:
+                return "Unknown"
         return dur
 
     async def get_filepath(
@@ -83,7 +85,7 @@ class TeleAPI:
                     )
                 )
             except:
-                file_name = audio.file_unique_id + "." + ".ogg"
+                file_name = audio.file_unique_id + "." + "ogg"
             file_name = os.path.join(os.path.realpath("downloads"), file_name)
         if video:
             try:
@@ -96,7 +98,9 @@ class TeleAPI:
         return file_name
 
     async def download(self, _, message, mystic, fname):
-        left_time = {}
+        lower = [0, 8, 17, 38, 64, 77, 96]
+        higher = [5, 10, 20, 40, 66, 80, 99]
+        checker = [5, 10, 20, 40, 66, 80, 99]
         speed_counter = {}
         if os.path.exists(fname):
             return True
@@ -112,78 +116,67 @@ class TeleAPI:
                     [
                         [
                             InlineKeyboardButton(
-                                text="😐 ᴄᴀɴᴄᴇʟ",
+                                text="ᴄᴀɴᴄᴇʟ",
                                 callback_data="stop_downloading",
                             ),
                         ]
                     ]
                 )
-                if datetime.now() > left_time.get(message.id):
-                    percentage = current * 100 / total
-                    percentage = str(round(percentage, 2))
-                    speed = current / check_time
-                    eta = int((total - current) / speed)
-                    downloader[message.id] = eta
-                    eta = get_readable_time(eta)
-                    if not eta:
-                        eta = "0 sec"
-                    total_size = convert_bytes(total)
-                    completed_size = convert_bytes(current)
-                    speed = convert_bytes(speed)
-                    text = f"""
-**{MUSIC_BOT_NAME} ᴍᴇᴅɪᴀ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ**
-
-**sɪᴢᴇ:** {total_size}
-**ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ:** {completed_size} 
-**ᴩᴇʀᴄᴇɴᴛᴀɢᴇ:** {percentage[:5]}%
-
-**sᴩᴇᴇᴅ:** {speed}/s
-**ᴇᴛᴀ:** {eta}"""
-                    try:
-                        await mystic.edit_text(text, reply_markup=upl)
-                    except:
-                        pass
-                    left_time[message.id] = datetime.now() + timedelta(
-                        seconds=self.sleep
-                    )
+                percentage = current * 100 / total
+                percentage = str(round(percentage, 2))
+                speed = current / check_time
+                eta = int((total - current) / speed)
+                eta = get_readable_time(eta)
+                if not eta:
+                    eta = "0 sᴇᴄᴏɴᴅs"
+                total_size = convert_bytes(total)
+                completed_size = convert_bytes(current)
+                speed = convert_bytes(speed)
+                percentage = int((percentage.split("."))[0])
+                for counter in range(7):
+                    low = int(lower[counter])
+                    high = int(higher[counter])
+                    check = int(checker[counter])
+                    if low < percentage <= high:
+                        if high == check:
+                            try:
+                                await mystic.edit_text(
+                                    text=_["tg_1"].format(
+                                        app.mention,
+                                        total_size,
+                                        completed_size,
+                                        percentage[:5],
+                                        speed,
+                                        eta,
+                                    ),
+                                    reply_markup=upl,
+                                )
+                                checker[counter] = 100
+                            except:
+                                pass
 
             speed_counter[message.id] = time.time()
-            left_time[message.id] = datetime.now()
-
             try:
                 await app.download_media(
                     message.reply_to_message,
                     file_name=fname,
                     progress=progress,
                 )
-                await mystic.edit_text(
-                    "**ғɪʟᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ, ᴩʀᴏᴄᴇssɪɴɢ...**"
-                )
-                downloader.pop(message.id)
+                try:
+                    elapsed = get_readable_time(
+                        int(int(time.time()) - int(speed_counter[message.id]))
+                    )
+                except:
+                    elapsed = "0 sᴇᴄᴏɴᴅs"
+                await mystic.edit_text(_["tg_2"].format(elapsed))
             except:
-                await mystic.edit_text(_["tg_2"])
-
-        if len(downloader) > 10:
-            timers = []
-            for x in downloader:
-                timers.append(downloader[x])
-            try:
-                low = min(timers)
-                eta = get_readable_time(low)
-            except:
-                eta = "Unknown"
-            await mystic.edit_text(_["tg_1"].format(eta))
-            return False
+                await mystic.edit_text(_["tg_3"])
 
         task = asyncio.create_task(down_load())
-        lyrical[mystic.message_id] = task
+        config.lyrical[mystic.id] = task
         await task
-        downloaded = downloader.get(message.id)
-        if downloaded:
-            downloader.pop(message.id)
-            return False
-        verify = lyrical.get(mystic.message_id)
+        verify = config.lyrical.get(mystic.id)
         if not verify:
             return False
-        lyrical.pop(mystic.message_id)
+        config.lyrical.pop(mystic.id)
         return True
