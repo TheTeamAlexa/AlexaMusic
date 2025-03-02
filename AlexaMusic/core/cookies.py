4d1ac9f81bd85edf376f84644e12f78233681c38
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2025-03-02 19:11:24 +0530

    Better !!!

--- AlexaMusic/core/cookies.py
+++ AlexaMusic/core/cookies.py
@@ -6,37 +6,43 @@
 
 import os
 import requests
-from typing import Optional
+from concurrent.futures import ThreadPoolExecutor
 
 import config
 from ..logging import LOGGER
 
 
-def save_file(
-    pastebin_url: str, file_path: str = "cookies/cookies.txt"
-) -> Optional[str]:
+def fetch_content(url: str):
     try:
-        response = requests.get(pastebin_url)
+        response = requests.get(url)
         response.raise_for_status()
-
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-        with open(file_path, "w") as file:
-            file.write(response.text)
-        return file_path
-
+        return response.text
     except requests.exceptions.RequestException as e:
-        LOGGER(__name__).error(f"Error fetching from {pastebin_url}: {e}")
-        return None
+        LOGGER(__name__).error(f"Error fetching from {url}: {e}")
+        return ""
 
 
-def save_cookies() -> None:
+def save_file(content: str, file_path: str):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as file:
+        file.write(content)
+    return file_path
+
+
+def save_cookies():
     full_url: str = str(config.COOKIES)
     paste_id: str = full_url.split("/")[-1]
     pastebin_url: str = f"https://batbin.me/raw/{paste_id}"
 
-    file_path = save_file(pastebin_url)
-    if file_path and os.path.getsize(file_path) > 0:
-        LOGGER(__name__).info(f"Cookies saved successfully to {file_path}.")
+    with ThreadPoolExecutor() as executor:
+        future = executor.submit(fetch_content, pastebin_url)
+        content = future.result()
+
+    if content:
+        file_path = save_file(content, "cookies/cookies.txt")
+        if os.path.getsize(file_path) > 0:
+            LOGGER(__name__).info(f"Cookies saved successfully to {file_path}.")
+        else:
+            LOGGER(__name__).error("Failed to save cookies or the file is empty.")
     else:
-        LOGGER(__name__).error("Failed to save cookies or the file is empty.")
+        LOGGER(__name__).error("Failed to fetch cookies.")
