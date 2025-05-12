458ef5d5256d146eba9caa8ba001e2c7e3d13a69
KSKOP69 <KSKOP69@users.noreply.github.com>
2025-05-12 19:37:27 +0000

    Formate By @TheTeamAlexa
    Signed-off-by: TheTeamAlexa <TheTeamAlexa@users.noreply.github.com>

--- genstring.py
+++ genstring.py
@@ -4,11 +4,16 @@
 import asyncio
 from pyrogram import Client
 
+
 async def generate_and_send_string_session(api_id, api_hash):
     async with Client("my_account", api_id=api_id, api_hash=api_hash) as app:
         string_session = await app.export_session_string()
-        await app.send_message("me", f"<b>Your Pyrogram String Session:</b>\n\n<code>{string_session}</code>")
+        await app.send_message(
+            "me",
+            f"<b>Your Pyrogram String Session:</b>\n\n<code>{string_session}</code>",
+        )
         print("String session has been sent to your 'Saved Messages'.")
+
 
 if __name__ == "__main__":
     api_id = int(input("Enter your API ID: "))
